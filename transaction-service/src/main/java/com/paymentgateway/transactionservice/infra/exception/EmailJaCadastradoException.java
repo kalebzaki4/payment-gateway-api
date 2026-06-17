3369e36801c8756a21dc3a9ba0915df57cb45262
anesiamattos <109079500+kalebzaki4@users.noreply.github.com>
2026-06-17 17:37:25 -0300

    Create EmailJaCadastradoException.java

---
+++ transaction-service/src/main/java/com/paymentgateway/transactionservice/infra/exception/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package com.paymentgateway.transactionservice.infra.exception;
+
+public class EmailJaCadastradoException extends RuntimeException {
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
